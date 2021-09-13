119bb78ca1545792bd45fa172333fd4529e48c79
Luke Hudson <lucas@speak.geek.nz>
2021-09-13 18:22:50 +1200

    feat: update generateion code

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function loadWords() {
   const files = [
     loadWordsFile('./words_ag.json'),
     loadWordsFile('./words_ho.json'),
-    loadWordsFile('./words_pz.json')
+    loadWordsFile('./words_pz.json'),
   ];
   return files.reduce((akk, wf) => akk.concat(wf));
 }
@@ -17,33 +17,35 @@ function loadWords() {
 function mapWord(w) {
   return {
     stems: cheerio.load(w[0]).text()
-      .split(/\s*,\s*/).map(s => s.replace(/[^a-z]*-[^a-z]*$/gi, '')),
+      .trim()
+      .split(/\s*,\s*/)
+      .map((s) => s.replace(/[^a-z]*-[^a-z]*$/gi, '')),
     meaning: cheerio.load(w[1] || '').text(),
     rawStems: w[0],
     rawMeaning: w[1],
     language: w[2],
     dictionaryLink: w[3],
     rawExamples: w[4],
-    examples: cheerio.load(w[4]||'').text().trim().split(/\s*,\s*/),
-  }
+    examples: cheerio.load(w[4] || '').text().trim().split(/\s*,\s*/),
+  };
 }
 
 const words = loadWords().map(mapWord);
 const index = words.reduce((akk, w) => {
-  w.stems.forEach(stem => akk.set(stem, w));
+  w.stems.forEach((stem) => akk.set(stem, w));
   return akk;
 }, new Map());
 const reverseIndex = words.reduce((akk, w) => {
-  w.meaning.split(/\s+/).forEach(word => {
+  w.meaning.split(/\s+/).forEach((word) => {
     if (!akk.has(word)) {
-        akk.set(word, [])
+      akk.set(word, []);
     }
     akk.get(word).push(w);
     return akk;
   });
-  w.examples.forEach(word => {
+  w.examples.forEach((word) => {
     if (!akk.has(word)) {
-        akk.set(word, [])
+      akk.set(word, []);
     }
     akk.get(word).push(w);
     return akk;
@@ -54,4 +56,6 @@ const reverseIndex = words.reduce((akk, w) => {
 // console.log('loadWords()', words);
 
 // console.log('index', reverseIndex.keys());
-console.log(reverseIndex.get(process.argv[2]));
+// console.log(reverseIndex.get(process.argv[2]));
+
+process.stdout.write(JSON.stringify(words, 0, 4));
