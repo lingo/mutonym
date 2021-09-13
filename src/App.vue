be858bfe4d9da5cd19e3c48b36cfa10aff368e23
Luke Hudson <lucas@speak.geek.nz>
2021-09-13 20:31:44 +1200

    feat: add styled title

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
   <main id="app">
+    <header>
+      <h1>Muto<span class="name">nym</span></h1>
+      <h2 class="subtitle">Greek and Latin roots in English</h2>
+    </header>
     <search :value="search" @input="onSearchChange($event)" />
-    <hr>
+    <hr />
     <div class="words">
-      <word-component v-for="w in found" :key="w.stems.join(':') + w.id" :word="w" />
+      <word-component
+        v-for="w in found"
+        :key="w.stems.join(':') + w.id"
+        :word="w"
+      />
     </div>
   </main>
 </template>
@@ -15,7 +23,7 @@ import Search from '@/components/Search.vue';
 import rawWords from '../gklatwords.json';
 
 // eslint-disable-next-line @typescript-eslint/ban-types
-export function debounce(fn: Function, delay: number) : () => void {
+export function debounce(fn: Function, delay: number): () => void {
   let timeoutID = 0;
 
   // eslint-disable-next-line func-names, @typescript-eslint/no-explicit-any
@@ -70,7 +78,7 @@ export default class App extends Vue {
     }).filter((w) => w.score > 0).sort((a, b) => (b.score - a.score));
   }
 
-  updateSearch(search: string) : void {
+  updateSearch(search: string): void {
     console.log('update search', search);
     this.search = search;
   }
@@ -78,16 +86,48 @@ export default class App extends Vue {
 </script>
 
 <style lang="scss">
-#app {
+@import url("https://fonts.googleapis.com/css2?family=Cinzel+Decorative:wght@700&family=Heebo:wght@100&display=swap");
+
+$green: #42b983;
+
+body {
+  color: #2c3e50;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+}
+
+#app {
   margin: 0 auto;
   margin-top: 60px;
   max-width: 80vw;
 }
 
+header {
+  display: flex;
+  align-items: baseline;
+  gap: 1rem;
+}
+h1 {
+  font-family: "Cinzel Decorative", serif;
+  font-size: 6rem;
+  line-height: 0.8;
+  margin: 1rem 0 2rem 0;
+  font-variant: small-caps;
+
+  span {
+    color: $green;
+    font-variant: normal;
+    font-size: 5.2rem;
+  }
+}
+h2.subtitle {
+  font-family: "Heebo", sans-serif;
+  font-size: 1.8rem;
+  letter-spacing: -0.1rem;
+  display: inline-block;
+  color: #282f38;
+}
 .words {
   display: flex;
   flex-flow: row wrap;
